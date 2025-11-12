24a1c195ed73ed41926471e31e8ed5348d76c9f2
Peter Anguelov <133042811+PeterAnguelov@users.noreply.github.com>
2025-11-12 20:05:05 +0200

    Commit 4
    StringFormating
    StringInterpolation
    SwitchStatements
    TryParse
    Var
    WhileLoops

---
+++ StringInterpolation/StringInterpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInterpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            int age = 23;
+
+            Console.WriteLine("Your name is " + name + " your age is " + age);
+            Console.WriteLine($"Your name is {name} your age is {age}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+
+            //while (i < 10)
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //}
+
+            Console.Write("Enter the first number: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int answer = numberA * numberB;
+            int actualAnswer = 0;
+
+            Console.Write($"Whats the value of {numberA} x {numberB}?");
+            Console.WriteLine();
+
+            /*while (answer != actualAnswer)
+            {
+                Console.Write("Enter your answer: ");
+                actualAnswer = Convert.ToInt32(Console.ReadLine());
+
+                if (answer != actualAnswer)
+                {
+                    Console.WriteLine("Wrong!");
+                    Console.WriteLine();
+                }
+            }*/
+
+            do
+            {
+                Console.Write("Enter your answer: ");
+                actualAnswer = Convert.ToInt32(Console.ReadLine());
+
+                if (answer != actualAnswer)
+                {
+                    Console.WriteLine("Wrong!");
+                    Console.WriteLine();
+                }
+            } while (answer != actualAnswer);
+
+            Console.WriteLine("Well done!");
+
+            Console.ReadLine();
+        }
+    }
+}
