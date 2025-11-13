48b6aa017ece8c9916db98d910eca099b948fae2
Peter Anguelov <133042811+PeterAnguelov@users.noreply.github.com>
2025-11-13 23:13:01 +0200

    Commit 6
    StringEqualsFunction

---
+++ StringEqualsFunction/StringEqualsFunction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEqualsFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello";
+            string compare = "Hello";
+
+            if (message.Equals(compare))
+            {
+                Console.WriteLine("same");
+            }
+            else
+            {
+                Console.WriteLine("Diff");
+            }
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            if (!name.Equals(""))
+            {
+                Console.WriteLine("Your name is " + name);
+            }
+            else
+            {
+                Console.WriteLine("Invalid name input");
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
---
+++ StringEqualsFunction/StringEqualsFunction/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringEqualsFunction")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringEqualsFunction")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3fe4b12f-8ce6-4dbb-8c71-93c8ddf646d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
