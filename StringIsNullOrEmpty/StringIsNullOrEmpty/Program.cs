22007a72130d81c5981111cafd14c17e5a6028a6
Peter Anguelov <133042811+PeterAnguelov@users.noreply.github.com>
2025-11-14 06:05:19 +0200

    Commit 7
    StringIsNullOrEmpty
    Exercise_PrintStringInReverse

---
+++ StringIsNullOrEmpty/StringIsNullOrEmpty/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringIsNullOrEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name}");
+
+            //if (name.Equals(""))
+            //{
+            //    Console.WriteLine("0");
+            //}
+
+            //if (!name.Equals(""))
+            //{
+            //    Console.WriteLine("1");
+            //}
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                if (!name.Equals(""))
+                {
+                    Console.WriteLine("2");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
