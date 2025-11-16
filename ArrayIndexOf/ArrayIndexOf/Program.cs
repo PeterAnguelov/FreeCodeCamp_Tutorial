b635218e79dd9dfbfee18c44999ef084be2f1476
Peter Anguelov <133042811+PeterAnguelov@users.noreply.github.com>
2025-11-16 09:38:42 +0200

    Commit 10
    ArrayIndexOf
    Lists

---
+++ ArrayIndexOf/ArrayIndexOf/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayIndexOf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                90, 199, 22, 50, 30
+            };
+
+            Console.Write("Enter number to search: ");
+            int search = Convert.ToInt32(Console.ReadLine());
+
+            //int position = Array.IndexOf(numbers, search);
+            //int position = Array.IndexOf(numbers, search, 2);
+            int position = Array.IndexOf(numbers, search, 1, 2);
+
+            if (position > -1)
+            {
+                Console.WriteLine($"Number {search} has been found at position {position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} has not been found"); 
+            }
+
+            /*int position = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == search)
+                {
+                    position = i;
+                }
+            }
+
+            if (position > -1)
+            {
+                Console.WriteLine($"Number {search} has been found at position {position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {search} has not been found");
+            }*/
+        }
+    }
+}
