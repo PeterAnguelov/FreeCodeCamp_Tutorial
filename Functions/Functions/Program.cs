485b0b184876113a40ccadd55d4efc6dc2138360
Peter Anguelov <133042811+PeterAnguelov@users.noreply.github.com>
2025-11-16 23:14:33 +0200

    Commit 12
    Functions

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Test
+    {
+        public void something()
+        {
+            Console.WriteLine();
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Test test = new Test();
+            test.something();
+            WelcomeMessage();
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome Aba");
+        }
+    }
+}
