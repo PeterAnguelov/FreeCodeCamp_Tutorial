d9e9ec3f73497f4e5d51e2d481a2b458f7f83bb6
Peter Anguelov <133042811+PeterAnguelov@users.noreply.github.com>
2025-11-20 22:22:02 +0200

    Commit 17
    ExceptionHandling
    PrintingErrorMessages

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // System.FormatException
+            // System.OverflowException
+
+            bool looping = true;
+            while (looping) 
+            {
+                try
+                {
+                    Console.Write("Enter a number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(num);
+                    looping = false;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Please enter a number less than 2 billion");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please only enter numbers");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+            
+        }
+    }
+}
---
+++ PrintingErrorMessages/PrintingErrorMessages/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingErrorMessages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only enter numbers");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter a value under 2 billion!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
